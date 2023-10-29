b20480039b18b0a869467fefa09a457b79b61ddd
Giorgia Ipsaro Passione <145654021+GIPSaro@users.noreply.github.com>
2023-10-29 11:00:30 +0100

    commit 7

--- Pagina Medium/assets/css/script.js
+++ Pagina Medium/assets/css/script.js
@@ -4,3 +4,17 @@ setInterval(() => {
   let random = Math.round(Math.random() * (M.length - 1));
   M[random].classList.toggle("invisible");
 }, 100);
+
+/* funzione per trasformare allo scrool la navbar bianca e bottone verde*/
+window.addEventListener("scroll", function () {
+  let navBar = document.getElementById("navContainer");
+  let btn = document.getElementById("btnGetStarted");
+  let eventScroll = window.scrollY;
+  if (eventScroll > 200) {
+    navBar.style.backgroundColor = "white";
+    btn.style.backgroundColor = `#1a8917`;
+  } else {
+    navBar.style.backgroundColor = `#ffc017`;
+    btn.style.backgroundColor = `#191919`;
+  }
+});
